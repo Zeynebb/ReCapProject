1c88fc75f5f53abdd322ec838734dc3ef5af7bb9
ZeynebEdaYILMAZ <zeynebb_edaa@hotmail.com>
2021-11-11 22:43:00 +0300

    Day7Homework2 - 11/11/2021
    Added Business, DataAccess, Entities and ConsoleUI layers
    Added Car class
    Added InMemoryCarDal class
    Testing InMemoryCarDal class methods

---
+++ ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1, BrandId=1,ColorId=2,DailyPrice=400, Description="Spor Araba", ModelYear=2018},
+                new Car{CarId=2, BrandId=2,ColorId=3,DailyPrice=700, Description="Limuzin", ModelYear=2020}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c=> c.CarId == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.BrandId = car.BrandId;
+
+        }
+    }
+}
